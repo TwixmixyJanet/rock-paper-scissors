8efe8ade91a9ad4527f313ca0d67643a85c3a8b2
Janet Webster <twixmixy@gmail.com>
2023-11-12 21:18:33 -0500

    Got the UI version working!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,39 +2,63 @@ let wins = 0;
 let ties = 0;
 let losses = 0;
 
-const options = ["R", "P", "S"];
+const copmuterOptions = ["R", "P", "S"];
+const userOptions = document.getElementById('options');
 
 const gameRound = document.getElementById('game-round-choice');
 const gameRoundChoice = document.getElementById('game-round-result');
 const results = document.getElementById('results');
-const userChoice = document.getElementById('user-choice');
+// const userChoiceForm = document.getElementById('user-choice');
+const R = document.getElementById('R');
+const P = document.getElementById('P');
+const S = document.getElementById('S');
 
+function clickedRock(e) {
+    e.preventDefault();
+    console.log("rock clicked");
+    let userChoice = "R"
+    console.log(userChoice);
+    playGame(userChoice);
+}
 
-const playGame = function() {
+function clickedPaper(e) {
+    e.preventDefault();
+    console.log("paper clicked");
+    let userChoice = "P"
+    console.log(userChoice);
+    playGame(userChoice);
+}
+
+function clickedScissors(e) {
+    e.preventDefault();
+    console.log("scissors clicked");
+    let userChoice = "S"
+    console.log(userChoice);
+    playGame(userChoice);
+}
+
+const playGame = function(userClicked) {
     console.log("play game")
-    let userChoice = prompt("Enter R, P, or S:");
-    if (!userChoice) {
-        return;
-    }
-    userChoice = userChoice.toUpperCase();
+    // userChoice = userChoice.toUpperCase();
+    console.log(userClicked)
 
-    let index = Math.floor(Math.random() * options.length);
-    let computerChoice = options[index];
+    let index = Math.floor(Math.random() * copmuterOptions.length);
+    let computerChoice = copmuterOptions[index];
 
     gameRoundChoice.innerHTML = `<p>
-    You chose ${userChoice}<br />
+    You chose ${userClicked}<br />
     The computer chose ${computerChoice}
     </p>`;
 
-    if (userChoice === computerChoice) {
+    if (userClicked === computerChoice) {
         ties++;
         gameRound.innerHTML = `<p>
         It's a tie!
         </p>`;
     } else if (
-        (userChoice === "R" && computerChoice === "S") ||
-        (userChoice === "P" && computerChoice === "R") ||
-        (userChoice === "S" && computerChoice === "P")
+        (userClicked === "R" && computerChoice === "S") ||
+        (userClicked === "P" && computerChoice === "R") ||
+        (userClicked === "S" && computerChoice === "P")
     ) {
         wins++;
         gameRound.innerHTML = `<p>
@@ -52,9 +76,13 @@ const playGame = function() {
     Wins: ${wins}<br />
     Losses: ${losses}<br />
     Ties: ${ties}<br />
-
-    Play again?
+    <br />
+    <b>Click the options above to play again!</b>
     </p>`;
 };
 
-document.getElementById("play-game").addEventListener("click", playGame);
+// document.getElementById("play-game").addEventListener("click", playGame);
+// document.getElementById('options').addEventListener('click', playGame);
+R.addEventListener('click', clickedRock);
+P.addEventListener('click', clickedPaper);
+S.addEventListener('click', clickedScissors);
